fe82ff3c389a63a19d6e2a44b26b3921708215f5
SnaksiXD <ataunimaa@gmail.com>
2021-09-21 18:15:03 +0300

    game scene aloitettu

--- Jalkapallo-Multiplayer/Assets/script/CreateAndJoinRoom.cs
+++ Jalkapallo-Multiplayer/Assets/script/CreateAndJoinRoom.cs
@@ -9,12 +9,22 @@ public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
 {
     public TMP_InputField createInput;
 
-    public TMP_InputField joinImput;
+    public TMP_InputField joinInput;
     // Start is called before the first frame update
     public void CreateRoom()
     {
         //Huoneen luonti
         PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
     }
 
 }
